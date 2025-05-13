263a218c34216ab0e19a2bc12340a50f08f820b4
fernandocamus <fe.camusr@duocuc.cl>
2025-05-13 00:45:38 -0400

    Creacion y subida de proyecto springboot

---
+++ src/test/java/com/perfulandia/perfulandiaSPA_EF1/PerfulandiaSpaEf1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandiaSPA_EF1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaSpaEf1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
