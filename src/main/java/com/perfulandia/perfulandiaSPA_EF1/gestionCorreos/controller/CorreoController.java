400ea674f0cdc66c56d867b15115088e63975c75
fernandocamus <fe.camusr@duocuc.cl>
2025-05-16 01:46:41 -0400

    Funcionalidad de envio de correos basica

--- src/main/java/com/perfulandia/perfulandiaSPA_EF1/gestionCorreos/controller/CorreoController.java
+++ src/main/java/com/perfulandia/perfulandiaSPA_EF1/gestionCorreos/controller/CorreoController.java
@@ -3,7 +3,6 @@ package com.perfulandia.perfulandiaSPA_EF1.gestionCorreos.controller;
 import com.perfulandia.perfulandiaSPA_EF1.gestionCorreos.model.Correo;
 import com.perfulandia.perfulandiaSPA_EF1.gestionCorreos.service.CorreoService;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
